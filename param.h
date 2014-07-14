a3c08466427d84714d28d72a0bc836dd9070be0c
drumber-1 <drumber01@gmail.com>
2014-07-14 16:51:24 +0100

    added controlled setup flag

--- param.h
+++ param.h
@@ -5,7 +5,9 @@
 #ifdef MAIN
 double albedo, opacity;
 std::string data_location;
+bool controlled_setup;
 #else
 extern double albedo, opacity;
 extern std::string data_location;
+extern bool controlled_setup;
 #endif
