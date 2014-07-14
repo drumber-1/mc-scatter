efb456a065c7af930448c593385ddd70c7e56e9f
drumber-1 <drumber01@gmail.com>
2014-07-14 16:53:05 +0100

    Added int to string function

--- util.cpp
+++ util.cpp
@@ -3,17 +3,25 @@
 #include <sstream>
 #include <iomanip>
 
-using namespace std;
-
 double toRad(double angle);
-string int_to_string(int x);
+std::string int_to_string(int x);
+double string_to_double(const std::string& s);
 
 double toRad(double angle){
 	return angle*M_PI/180.0;
 }
 
-string int_to_string(int n, int width){
-	stringstream sstm;
-	sstm << setw(width) << setfill('0') << n;
+std::string int_to_string(int n, int width){
+	std::stringstream sstm;
+	sstm << std::setw(width) << std::setfill('0') << n;
 	return sstm.str();
+}
+
+double string_to_double(const std::string& s){
+	std::istringstream i(s);
+	double x;
+	if(!(i >> x)){
+		return 0;
+	}
+	return x;
 }
