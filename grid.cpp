916d81f9b47dd7ae758249599a93d189dc38596d
drumber-1 <drumber01@gmail.com>
2014-07-29 17:43:15 +0100

    Added ability to create empty and invalid grids

--- grid.cpp
+++ grid.cpp
@@ -13,7 +13,16 @@
 
 extern std::string int_to_string(int x, int width);
 
-Grid::Grid(const GridParameters& gp){
+Grid::Grid(bool grid_is_valid) : good(grid_is_valid) {
+	for (int i = 0; i < 3; i++) {
+		parameters.ncells[i] = 0;
+		parameters.left_boundary[i] = 0.0;
+		parameters.right_boundary[i] = 0.0;
+	}
+}
+
+
+Grid::Grid(const GridParameters& gp) : good(true) {
 	parameters = gp;
 	
 	for(int i = 0; i < 3; i++){
@@ -42,6 +51,34 @@ Grid::Grid(const GridParameters& gp){
 	
 }
 
+Grid::Grid(const Grid& other) : good(other.good){
+	parameters = GridParameters(other.parameters);
+	
+	for(int i = 0; i < 3; i++){
+		spacing[i] = (parameters.right_boundary[i] - parameters.left_boundary[i])/parameters.ncells[i];
+	}
+	
+	rho_data = new double **[parameters.ncells[0]];
+	
+	for(int i = 0; i < parameters.ncells[0]; i++){
+		rho_data[i] = new double *[parameters.ncells[1]];
+	}
+	
+	for(int i = 0; i < parameters.ncells[0]; i++){
+		for(int j = 0; j < parameters.ncells[1]; j++){
+			rho_data[i][j] = new double [parameters.ncells[2]];
+		}
+	}
+	
+	for(int i = 0; i < parameters.ncells[0]; i++){
+		for(int j = 0; j < parameters.ncells[1]; j++){
+			for(int k = 0; k < parameters.ncells[2]; k++){
+				rho_data[i][j][k] = other.rho_data[i][j][k];
+			}
+		}
+	}
+}
+
 Grid::~Grid(){
 	for(int i = 0; i < parameters.ncells[0]; i++){
 		for(int j = 0; j < parameters.ncells[1]; j++){
@@ -60,6 +97,14 @@ void Grid::output_slices(std::string data_location, unsigned int sliced_dim) con
 	if(sliced_dim > 2){
 		if(procRank == 0){
 			std::cerr << "Cannot slice across dimension " << sliced_dim << std::endl;
+		}
+		return;
+	}
+	
+	if(!good){
+		//Possible exception here, as this shouldn't happen
+		if(procRank == 0){
+			std::cerr << "Cannot slice, grid is flagged as bad" << std::endl;
 		}
 		return;
 	}
@@ -106,63 +151,78 @@ void Grid::output_slices(std::string data_location, unsigned int sliced_dim) con
 	}
 }
 
-bool Grid::is_on_grid(std::vector<double> pos) const {
-	for(int i = 0; i < 3; i++){
-		if(pos[i] >= parameters.right_boundary[i] || pos[i] <= parameters.left_boundary[i]){
-			return false;
-		}
+bool Grid::is_on_grid(const std::vector<double>& pos) const {
+	if (pos[0] <= parameters.left_boundary[0] ||
+	    pos[1] <= parameters.left_boundary[1] ||
+	    pos[2] <= parameters.left_boundary[2] ||
+	    pos[0] >= parameters.right_boundary[0] ||
+	    pos[1] >= parameters.right_boundary[1] ||
+	    pos[2] >= parameters.right_boundary[2]) {
+		return false;
+	} else {
+		return true;
 	}
-	return true;
 }
 
-std::vector<double> Grid::get_position(std::vector<int> cell) const {
+bool Grid::is_on_grid(const std::vector<int>& cell) const {
+	if (cell[0] < 0 ||
+	    cell[1] < 0 ||
+	    cell[2] < 0 ||
+	    cell[0] >= parameters.ncells[0] ||
+	    cell[1] >= parameters.ncells[1] ||
+	    cell[2] >= parameters.ncells[2]) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+std::vector<double> Grid::get_position(const std::vector<int>& cell) const {
 	std::vector<double> pos(3);
-	for(int i = 0; i < 3; i++){
+	for (int i = 0; i < 3; i++) {
 		pos[i] = cell[i]*spacing[i] + parameters.left_boundary[i];
 	}
 	return pos;
 }
 
-std::vector<int> Grid::get_cell(std::vector<double> pos) const {
+std::vector<int> Grid::get_cell(const std::vector<double>& pos) const {
 	std::vector<int> cell(3);
-	for(int i = 0; i < 3; i++){
+	for (int i = 0; i < 3; i++) {
 		cell[i] = floor((pos[i] - parameters.left_boundary[i])/spacing[i]);
 	}
 	return cell;
 }
 
-double Grid::get_rho(std::vector<double> pos) const {
+double Grid::get_rho(const std::vector<double>& pos) const {
 	std::vector<int> cell = get_cell(pos);
-
-	for(int i = 0; i < 3; i++){
-		if(cell[i] < 0){
-			cell[i] = 0;
-		} else if(cell[i] >= parameters.ncells[i]){
-			cell[i] = parameters.ncells[i] - 1;
-		}
-	}
-	
 	return get_rho(cell);
 }
 
-double Grid::get_rho(std::vector<int> cell) const {
-	for(int i = 0; i < 3; i++){
-		if(cell[i] < 0){
-			cell[i] = 0;
-		} else if(cell[i] >= parameters.ncells[i]){
-			cell[i] = parameters.ncells[i] - 1;
-		}
+double Grid::get_rho(const std::vector<int>& cell) const {
+	if (is_on_grid(cell)) {
+		return rho_data[cell[0]][cell[1]][cell[2]];
+	} else {
+		return 0.0;
 	}
-	return rho_data[cell[0]][cell[1]][cell[2]];
 }
 
-void Grid::set_rho(std::vector<int> cell, double rho){
-	for(int i = 0; i < 3; i++){
-		if(cell[i] < 0 || cell[i] >= parameters.ncells[i]){
-			return;
-		} else {
-			rho_data[cell[0]][cell[1]][cell[2]] = rho;
-		}
+void Grid::set_rho(const std::vector<int>& cell, double rho) {
+	if (is_on_grid(cell)) {
+		rho_data[cell[0]][cell[1]][cell[2]] = rho;
 	}
 }
+
+double Grid::get_spacing(int dim) const{
+	return spacing[dim];
+}
+
+bool Grid::is_good() const {
+	return good;
+}
+
+GridParameters Grid::get_parameters() const {
+	return parameters;
+}
+
+
 
--- grid.h
+++ grid.h
@@ -15,15 +15,22 @@ class Grid {
 		GridParameters parameters;
 		double ***rho_data;
 		double spacing[3];
+		const bool good; //This set false when a grid is expected to be returned, but a sensible grid cannot be created
 	public:
 		Grid(const GridParameters&);
+		Grid(bool);
 		~Grid();
 		Grid(const Grid&);
 		void output_slices(std::string, unsigned int) const;
-		bool is_on_grid(std::vector<double> pos) const;
-		std::vector<double> get_position(std::vector<int> cell) const;
-		std::vector<int> get_cell(std::vector<double> pos) const;
-		double get_rho(std::vector<double> pos) const;
-		double get_rho(std::vector<int> cell) const;
-		void set_rho(std::vector<int> cell, double rho);
+		bool is_on_grid(const std::vector<double>&) const;
+		bool is_on_grid(const std::vector<int>&) const;
+		std::vector<double> get_position(const std::vector<int>&) const;
+		std::vector<int> get_cell(const std::vector<double>&) const;
+		double get_rho(const std::vector<double>&) const;
+		double get_rho(const std::vector<int>&) const;
+		void set_rho(const std::vector<int>&, double rho);
+		double get_spacing(int dim) const;
+		bool is_good() const;
+		GridParameters get_parameters() const;
 };
+
