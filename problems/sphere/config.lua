b4d2954927b6cb6bddf1233e0f10c67643947eba
drumber-1 <drumber01@gmail.com>
2014-11-28 14:32:09 +0000

    Can read files from working directory or data dir with -d flag

--- problems/sphere/config.lua
+++ problems/sphere/config.lua
@@ -2,7 +2,7 @@ data_location = "./data"
 colden_location = "./data/colden";
 scatter_location = "./data/scatter";
 
-grid_cells = {500, 500, 500}
+grid_cells = {50, 50, 50}
 grid_left = {-2.0, -2.0, -2.0}
 grid_right = {2.0, 2.0, 2.0}
 
